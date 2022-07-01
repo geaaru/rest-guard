200fc6be5b7590fcbfcab2814760de5b2ba19b0b
Daniele Rondina <geaaru@gmail.com>
2022-07-01 22:25:50 +0200

    specs: Add tags for viper

--- pkg/specs/definition.go
+++ pkg/specs/definition.go
@@ -9,41 +9,41 @@ import (
 )
 
 type RestTicket struct {
-	Id          string
-	Request     *http.Request
-	Response    *http.Response
-	Path        string
-	Retries     int
-	Service     *RestService
-	Node        *RestNode
-	FailedNodes RestNodes
+	Id          string         `json:"id,omitempty" yaml:"id,omitempty" mapstructure:"id,omitempty"`
+	Request     *http.Request  `json:"req,omitempty" yaml:"req,omitempty" mapstructure:"req,omitempty"`
+	Response    *http.Response `json:"resp,omitempty" yaml:"resp,omitempty" mapstructure:"resp,omitempty"`
+	Path        string         `json:"path,omitempty" yaml:"path,omitempty" mapstructure:"path,omitempty"`
+	Retries     int            `json:"retries,omitempty" yaml:"retries,omitempty" mapstructure:"retries,omitempty"`
+	Service     *RestService   `json:"service,omitempty" yaml:"service,omitempty" mapstructure:"service,omitempty"`
+	Node        *RestNode      `json:"node,omitempty" yaml:"node,omitempty" mapstructure:"node,omitempty"`
+	FailedNodes RestNodes      `json:"failed_node,omitempty" yaml:"failed_node,omitempty" mapstructure:"failed_node,omitempty"`
 }
 
 type RestNode struct {
-	Name    string `json:"name" yaml:"name"`
-	BaseUrl string `json:"base_url" yaml:"base_url"`
-	Ssl     bool   `json:"ssl,omitempty" yaml:"ssl,omitempty"`
+	Name    string `json:"name" yaml:"name" mapstructure:"name"`
+	BaseUrl string `json:"base_url" yaml:"base_url" mapstructure:"base_url"`
+	Ssl     bool   `json:"ssl,omitempty" yaml:"ssl,omitempty" mapstructure:"ssl,omitempty"`
 }
 
 type RestNodes []*RestNode
 
 type RestService struct {
-	Name            string      `json:"name" yaml:"name"`
-	Nodes           []*RestNode `json:"nodes" yaml:"nodes"`
-	Retries         int         `json:"retries,omitempty" yaml:"retries,omitempty"`
-	RetryIntervalMs int         `json:"retry_interval_ms,omitempty" yaml:"retry_interval_ms,omitempty"`
+	Name            string      `json:"name" yaml:"name" mapstructure:"name"`
+	Nodes           []*RestNode `json:"nodes" yaml:"nodes" mapstructure:"nodes"`
+	Retries         int         `json:"retries,omitempty" yaml:"retries,omitempty" mapstructure:"retries,omitempty"`
+	RetryIntervalMs int         `json:"retry_interval_ms,omitempty" yaml:"retry_interval_ms,omitempty" mapstructure:"retry_interval_ms,omitempty"`
 
-	RespValidatorCb func(t *RestTicket) bool
+	RespValidatorCb func(t *RestTicket) bool `json:"-" yaml:"-" mapstructure:"-"`
 }
 
 type RestGuardConfig struct {
-	UserAgent string `json:"user_agent,omitempty" yaml:"user_agent,omitempty"`
+	UserAgent string `json:"user_agent,omitempty" yaml:"user_agent,omitempty" mapstructure:"user_agent,omitempty,omitempty"`
 
-	ReqsTimeout         int  `json:"reqs_timeout,omitempty" yaml:"reqs_timeout,omitempty"`
-	MaxIdleConns        int  `json:"max_idle_conns,omitempty" yaml:"max_idle_conns,omitempty"`
-	IdleConnTimeout     int  `json:"idle_conn_timeout,omitempty" yaml:"idle_conn_timeout,omitempty"`
-	MaxConnsPerHost     int  `json:"max_conns4host,omitempty" yaml:"max_conns4host,omitempty"`
-	MaxIdleConnsPerHost int  `json:"max_idleconns4host,omitempty" yaml:"max_idleconns4host,omitempty"`
-	DisableCompression  bool `json:"disable_compression,omitempty" yaml:"disable_compression,omitempty"`
-	InsecureSkipVerify  bool `json:"insecure_skip_verify,omitempty" yaml:"insecure_skip_verify,omitempty"`
+	ReqsTimeout         int  `json:"reqs_timeout,omitempty" yaml:"reqs_timeout,omitempty" mapstructure:"reqs_timeout,omitempty"`
+	MaxIdleConns        int  `json:"max_idle_conns,omitempty" yaml:"max_idle_conns,omitempty" mapstructure:"max_idle_conns,omitempty"`
+	IdleConnTimeout     int  `json:"idle_conn_timeout,omitempty" yaml:"idle_conn_timeout,omitempty" mapstructure:"idle_conn_timeout,omitempty"`
+	MaxConnsPerHost     int  `json:"max_conns4host,omitempty" yaml:"max_conns4host,omitempty" mapstructure:"max_conns4host,omitempty"`
+	MaxIdleConnsPerHost int  `json:"max_idleconns4host,omitempty" yaml:"max_idleconns4host,omitempty" mapstructure:"max_idleconns4host,omitempty"`
+	DisableCompression  bool `json:"disable_compression,omitempty" yaml:"disable_compression,omitempty" mapstructure:"disable_compression,omitempty"`
+	InsecureSkipVerify  bool `json:"insecure_skip_verify,omitempty" yaml:"insecure_skip_verify,omitempty" mapstructure:"insecure_skip_verify,omitempty"`
 }
